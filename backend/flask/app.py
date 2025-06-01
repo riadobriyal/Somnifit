97b6bb85d01f28a0c8554931ee67aa0e57a7013c
Aryan3ROX <agaryan003@gmail.com>
2025-06-01 21:24:11 +0530

    fixed encoder for sleep

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -4,8 +4,7 @@ import numpy as np
 
 app = Flask(__name__)
 
-gender_encoder = pickle.load(open('../models/gender_encoder.pkl','rb'))
-scaler = pickle.load(open('../models/scaler_sleep.pkl','rb'))
+sleep_transformer = pickle.load(open('../models/column_transformer_sleep.pkl','rb'))
 lin_model = pickle.load(open('../models/lin_reg_sleep.pkl','rb'))
 
 @app.route("/")
@@ -20,12 +19,12 @@ def sleep_prediction():
     deep_sleep = float(data.get('deep_sleep'))
     awake_others_duration = float(data.get('awake_others_duration'))
     age = float(data.get('age'))
-    gender = int(gender_encoder.transform(np.array([data.get('gender')])))
+    gender = data.get('gender')
     bed_time_hours = float(data.get('bed_time_hours'))
     wakeup_time_hours = float(data.get('wakeup_time_hours'))
 
     input_data = np.array([[sleep_duration,deep_sleep,awake_others_duration,age,gender,bed_time_hours,wakeup_time_hours]])
-    prediction = lin_model.predict(scaler.transform(input_data))
+    prediction = lin_model.predict(sleep_transformer.transform(input_data))
 
     return jsonify({'sleep_efficiency': float(prediction)})
 
